c01a4491429d5ee5e6473c1a5c0c7cc9b1e5d14f
ethangmann1 <119371277+ethangmann1@users.noreply.github.com>
2022-11-29 12:01:49 -0500

    working code

--- guessing game.py
+++ guessing game.py
@@ -1,12 +1,12 @@
-i = "mazda miata"
+answer = "mazda miata"
 guess_count = 0
-guess_limit = 5
+guess_limit = 3
 
 while guess_count < guess_limit:
-    guess = int(input('Guess: '))
+    guess = (input('Guess: '))
     guess_count += 1
-    if guess == i:
-        print('Miata is always the answer')
+    if guess == answer:
+        print ('Miata is always the answer')
         break
 else:
     print('Wrong, try again')
